7d70a1958d97014d35d519174c12097eb29a74f8
sofeaedruce <sofea5488@gmail.com>
2017-02-03 14:22:24 +0800

    sofea

--- js/my-app.js
+++ js/my-app.js
@@ -853,14 +853,14 @@ function deleteJobOffered(id) {
 	});
 }
 
-function onSuccess(googleUser) {
+/* function onSuccess(googleUser) {
 	 
 	var profile = googleUser.getBasicProfile();
-	/* console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+	 console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
 	console.log('Name: ' + profile.getName());
 	console.log('Image URL: ' + profile.getImageUrl());
 	console.log('Email: ' + profile.getEmail());
-	 */
+	 
 	var user_fullname = profile.getName();
 	var user_pic = profile.getImageUrl();
 	var user_email = profile.getEmail();
@@ -892,7 +892,7 @@ function renderButton() {
 		  'onsuccess': onSuccess,
 		  'onfailure': onFailure
 	});
-}
+} */
 
 function book(id) {
 	var id = id;
